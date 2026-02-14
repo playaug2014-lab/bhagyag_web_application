842f657f7f6ff60760f03c9b0653eb969af871e7
sksandroiddeveloper <67684190+sksandroiddeveloper@users.noreply.github.com>
2026-02-14 11:53:47 +0530

    bhgyagweb
    New update

--- android/app/src/main/kotlin/com/bhagyag/astrologer/bhagyag/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.bhagyag.astrologer.bhagyag
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/main_web.dart
+++
@@ -1,4 +0,0 @@
-Future<void> initializeAppCheck() async {
-  print('✅ Running on WEB - App Check not needed');
-  // No-op for web
-}
